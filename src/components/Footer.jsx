b87bf5aa571c9f5cfe52e5e3553f4f701c63a02f
Schlumen <lumen.haendler@web.de>
2024-05-16 10:53:25 +0200

    Initial structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2024 Lumen Haendler</small>
+      <p>
+        Version <b>1.0</b>
+      </p>
+    </footer>
+  );
+}
